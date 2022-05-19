beae3ad1f1ed223334b72e697bf97376d7ce5853
Aklilu <aklilu.tsige.w@gmail.com>
2022-05-19 19:47:47 +0200

    Add component scan anotation

--- src/main/java/com/halehabank/halehabank/HalehaBankApplication.java
+++ src/main/java/com/halehabank/halehabank/HalehaBankApplication.java
@@ -2,8 +2,10 @@ package com.halehabank.halehabank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.halehabank.controller")
 public class HalehaBankApplication {
 
 	public static void main(String[] args) {
