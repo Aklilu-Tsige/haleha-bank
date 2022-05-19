e7ada14a338e98dffaddbbe07a2b7704325ffbb7
Aklilu <aklilu.tsige.w@gmail.com>
2022-05-19 19:26:14 +0200

    haleha Banck application init

---
+++ src/test/java/com/halehabank/halehabank/HalehaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halehabank.halehabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalehaBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
